5b379da4ef5e7e64d6121fbda5b6ac06e223e3d3
Djinn08 <connor.bohlin@gmail.com>
2025-08-31 00:23:55 -0500

    Added more curated sitemap change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,58 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 5000,
   outDir: "./public",
+  exclude: [
+    '/api/*',
+    '/_next/*',
+    '/admin/*',
+    '/private/*',
+    '/*.json',
+    '/*.xml',
+    '/legal/*',
+    '/offers/*',
+    '/process/*',
+    '/roadmaps/*',
+    '/services/*',
+  ],
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: '/',
+        changefreq: 'weekly',
+        priority: 1.0,
+      },
+      {
+        loc: '/about',
+        changefreq: 'monthly',
+        priority: 0.8,
+      },
+      {
+        loc: '/pricing',
+        changefreq: 'monthly',
+        priority: 0.9,
+      },
+      {
+        loc: '/start-project',
+        changefreq: 'monthly',
+        priority: 0.8,
+      },
+      {
+        loc: '/add-ons',
+        changefreq: 'monthly',
+        priority: 0.7,
+      },
+      {
+        loc: '/work',
+        changefreq: 'weekly',
+        priority: 0.6,
+      },
+      {
+        loc: '/contact',
+        changefreq: 'monthly',
+        priority: 0.7,
+      },
+    ];
+  },
 };
 
 
